c75e07727881e03f02df127c91f9b8d52bef08b1
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-11-22 11:11:29 +0100

    updated

---
+++ Literature/Preprocessing.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Loading images
+
+# In[71]:
+
+
+from PIL import Image
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from torch.utils.data import Dataset, DataLoader,TensorDataset
+from torchvision import transforms
+
+from torchsummary import summary
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[72]:
+
+
+#labels with the same order
+train_images = []
+train_labels = []
+
+test_images = []
+test_dict = {}
+train_filenames = []
+
+labels_df = pd.read_csv('train_onelabel.csv')
+labels_dict = labels_df.set_index('image')['class'].to_dict()
+
+for filename in labels_df['image'].values: ##to keep mapping with classes
+    train_images.append(Image.open('train_images/'+filename).copy())
+    train_labels.append(labels_dict[filename])
+    train_filenames.append(filename)
+for filename in glob.iglob('test_images' +'/*'):
+    image = Image.open(filename).copy()
+    test_images.append(image)
+    test_dict[filename.replace('test_images/', '')] = image
+
+
+# In[73]:
+
+
+widths, heights = [], [] 
+sumx, sumy = 0, 0
+for i in train_images:
+    sumx += i.size[0]
+    widths.append(i.size[0])
+    sumy += i.size[1]
+    heights.append(i.size[1])
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.hist(widths)
+ax2.hist(heights, color = 'orange')
+fig.set_size_inches(12, 5)
+
+avg_width = np.mean(widths)
+avg_height = np.mean(heights)
+print('Average width {} , Average height: {}'.format(avg_width, avg_height))
+
+
+# In[133]:
+
+
+train_transforms(train_images[0])
+
+
+# In[125]:
+
+
+class ListsDataset(Dataset):
+    def __init__(self, list_of_images, list_of_labels, transform=None):
+        """
+        Args:
+            csv_path (string): path to csv file
+            height (int): image height
+            width (int): image width
+            transform: pytorch transforms for transforms and tensor conversion
+        """
+#         super().__init__()
+        self.data = list_of_images
+        self.labels = np.asarray(list_of_labels).reshape(-1,1)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        single_image = self.data[index]
+        single_image_label = self.labels[index]
+        # Transform image to tensor
+        if self.transform is not None:
+            img_as_tensor = self.transform(single_image)
+        # Return image and the label
+        return (img_as_tensor, single_image_label)
+
+    def __len__(self):
+        return len(self.data)
+
+
+# In[132]:
+
+
+#Transforms and Dataset Creation
+test_transforms = transforms.ToTensor()
+train_transforms = transforms. Compose([
+    transforms.CenterCrop(28),
+    transforms.ToTensor()
+])
+
+train_dataset = ListsDataset(train_images, train_labels, transform = train_transforms)
+
+
+# In[94]:
+
+
+
+
+
+# In[134]:
+
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 32, shuffle = True)
+# test_loader = torch.utils.data.DataLoader(test_images, batch_size = 32, shuffle = True)
+
+
+# In[ ]:
+
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 50, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(50, 20, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+            Flatten(),
+            nn.Linear(20*7*7, 250),
+            nn.ReLU())
+        self.fc = nn.Linear(250, 121)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.fc(out)
+        return out
+
+
+# In[138]:
+
+
+# device = torch.device("cpu");
+#CNN architecture
+cnn = CNN().cuda()
+summary(cnn, (1,28,28))
+#Hyperparameters
+learning_rate = 0.01
+batch_size = 32
+
+
+# In[ ]:
+
+
+def train(net, num_epochs):
+    criterion = nn.CrossEntropyLoss();
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate);
+    #Training
+    history = {'batch': [], 'loss': [], 'accuracy': []}
+    for epoch in range(num_epochs):
+            for i, (images, labels) in enumerate(train_loader):
+                images = Variable(images).cuda()
+                labels = Variable(labels).squeeze(1).long().cuda()#.cpu()
+                # Forward + Backward + Optimize
+                optimizer.zero_grad()
+                outputs = cnn(images)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                _, argmax = torch.max(outputs, 1)
+                accuracy_train = (labels == argmax.squeeze()).float().mean()*100
+                # Show progress
+                if (i+1) % 32 == 0:
+                    log = " ".join([
+                      "Epoch : %d/%d" % (epoch+1, num_epochs),
+                      "Iter : %d/%d" % (i+1, len(train_dataset)//batch_size),
+                      "Loss: %.4f" % loss.item(),
+                      "Accuracy: %.4f" % accuracy_train])
+                    print('\r{}'.format(log), end='')
+                    history['batch'].append(i)
+                    history['loss'].append(loss.item())
+                    history['accuracy'].append(accuracy_train.item())
+            print()
+train(CNN, num_epochs = 50)
+
