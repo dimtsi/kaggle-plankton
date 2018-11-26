fc4a966962df7aa54dc17f5bb7ea817535d398a8
Dimitrios Tsimpoukis <dimitrio@pablo.is.cwi.nl>
2018-11-26 16:30:05 +0100

    nn

--- NNs.py
+++ NNs.py
@@ -15,9 +15,9 @@ class ResNetMine(nn.Module):
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(3, stride=1)
-        self.fc = nn.Linear(2304 * block.expansion, num_classes)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -53,7 +53,7 @@ class ResNetMine(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-#         x = self.layer4(x)
+        x = self.layer4(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
 
@@ -108,7 +108,7 @@ class CNN(nn.Module):
             nn.MaxPool2d(2))
         self.layer3 = nn.Sequential(
             Flatten(),
-            nn.Linear(20*7*7, 250),
+            nn.Linear(20*16*16, 250),
             nn.ReLU())
         self.fc = nn.Linear(250, 121)
 
