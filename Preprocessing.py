8aca91220e73d1cab21d52fc6a52a325843935c9
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-02 20:14:03 +0100

    nn

--- Preprocessing.py
+++ Preprocessing.py
@@ -345,7 +345,7 @@ def run_KFolds():
         trained_models.append(trained_model)
         break
 
-run_KFolds()
+# run_KFolds()
 # final_model = trained_models[0].eval().cuda()
 
 
@@ -367,7 +367,7 @@ def train_on_whole():
     model = train(cnn, train_loader, num_epochs=100)
     return model
 
-
+train_on_whole()
 #predict on testset
 # final_model = ResNetMine(Bottleneck, [3, 4, 2, 2])
 # final_model.load_state_dict(torch.load('trained_model.pt')['state_dict'])
