da80b7ccc6df0602edd574e1de706efc88cb498a
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-03 09:44:32 +0100

    new

--- NNs.py
+++ NNs.py
@@ -2,24 +2,62 @@ import torch
 import torch.nn as nn
 import math;
 
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.LeakyReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
 class ResNetMine(nn.Module):
 
     def __init__(self, block, layers, num_classes=121):
-        self.inplanes = 256
+        self.inplanes = 128
         super(ResNetMine, self).__init__()
-        self.conv1 = nn.Conv2d(1, 256, kernel_size=3, stride=2, padding=1,
+        self.conv1 = nn.Conv2d(1, 128, kernel_size=3, stride=2, padding=1,
                                bias=False)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.relu = nn.ReLU(inplace=True)
+        self.bn1 = nn.BatchNorm2d(128)
+        self.relu = nn.LeakyReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 128, layers[0])
-        self.layer2 = self._make_layer(block, 256, layers[1], stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         # self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         # self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Sequential(
-            nn.Linear(256*block.expansion, 64*block.expansion),
-            nn.ReLU(),
+            nn.Linear(128*block.expansion, 64*block.expansion),
+            nn.LeakyReLU(),
             # nn.Dropout(0.5),
             nn.Linear(64*block.expansion, num_classes)
         )
