ba8e3c1bb2480131615421e091f89e9a063cb240
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-03 09:47:43 +0100

    new

--- Preprocessing.py
+++ Preprocessing.py
@@ -345,7 +345,7 @@ def run_KFolds():
         trained_models.append(trained_model)
         break
 
-# run_KFolds()
+run_KFolds()
 # final_model = trained_models[0].eval().cuda()
 
 
@@ -369,8 +369,8 @@ def train_on_whole():
 
 # train_on_whole()
 #predict on testset
-final_model = ResNetMine(Bottleneck, [3, 4, 6, 3])
-final_model.load_state_dict(torch.load('trained_model.pt')['state_dict'])
+# final_model = ResNetMine(Bottleneck, [3, 4, 6, 3])
+# final_model.load_state_dict(torch.load('trained_model.pt')['state_dict'])
 def predict_test_set(model, filenames):
     test_transforms = transforms. Compose([
         transforms.Grayscale(),
@@ -393,4 +393,4 @@ def predict_test_set(model, filenames):
     results_df.to_csv('results.csv',sep = ',', index = False)
 
 # final_model
-predict_test_set(final_model, test_filenames)
+# predict_test_set(final_model, test_filenames)
