2b4a83444717895640fd8314cff2c6dba0ca1a89
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-05 14:12:49 +0100

    nn

--- Preprocessing.py
+++ Preprocessing.py
@@ -341,7 +341,7 @@ def run_KFolds():
             X_val.append(train_images[j])
             y_val.append(train_labels[j])
         train_loader, test_loader = create_datasets_dataloaders(
-            X_train, y_train, X_val, y_val, batch_size = 32)
+            X_train, y_train, X_val, y_val, batch_size = 128)
 
         #Training
         # cnn1 = resnet34(pretrained = True)
