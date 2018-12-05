a64ab60f98e76e19627c4ab323a8c342938488c1
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-05 12:40:59 +0100

    nns

--- NNs.py
+++ NNs.py
@@ -111,7 +111,7 @@ class SuperNet(nn.Module):
     def __init__(self, networks, num_classes=121):
         super(SuperNet, self).__init__()
         self.net1 =  nn.Sequential(*list(networks[0].children())[:-1])
-        self.net2 =  nn.Sequential(*list(networks[1].children())[:-1])
+        self.net2 =  nn.Sequential(*list(networks[1].children())[:-1]).cuda(1)
         self.fc = nn.Linear(128*4*2, num_classes)
         # if torch.cuda.device_count() > 1:
         #     print("2GPU")
@@ -123,8 +123,8 @@ class SuperNet(nn.Module):
 
     def forward(self, x):
         x1 = self.net1(x)
-        x2 = self.net2(x)
-        z = torch.cat((x1, x2), 1)
+        x2 = self.net2(x.cuda(1))
+        z = torch.cat((x1, x2.cuda(0)), 1)
         z = z.view(z.size(0), -1)
         z = self.fc(z)
         return z
