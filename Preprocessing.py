bfdf4118e1fe856d6044cec9f0636aeb3fa5469d
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-05 12:23:47 +0100

    time

--- Preprocessing.py
+++ Preprocessing.py
@@ -352,8 +352,8 @@ def run_KFolds():
         models.append(cnn1)
         models.append(cnn2)
 
-        cnn = ResNetMine(Bottleneck, [3, 4, 6, 3])
-        # cnn = SuperNet(models)
+        # cnn = ResNetMine(Bottleneck, [3, 4, 6, 3])
+        cnn = SuperNet(models)
         # if torch.cuda.device_count() > 1:
         #   print("Let's use", torch.cuda.device_count(), "GPUs!")
         #   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
