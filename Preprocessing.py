e45f72986a9c54d993569faed0b1e81fd4ffa4ea
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-05 19:04:57 +0100

    nn

--- Preprocessing.py
+++ Preprocessing.py
@@ -346,7 +346,7 @@ def run_KFolds():
             X_train, y_train, X_val, y_val, batch_size = 32)
 
         #Training
-        pretrained = resnet152(pretrained = True)
+        pretrained = resnet50(pretrained = True)
         # cnn1 = PretrainedResnetMine(pretrained.block, pretrained.layers,
         #  pretrained_nn = pretrained)
 
