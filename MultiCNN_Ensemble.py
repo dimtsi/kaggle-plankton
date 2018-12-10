51be630e82f7f32af07adc57e0590d538ba8bf93
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-10 15:20:05 +0100

    nn

--- MultiCNN_Ensemble.py
+++ MultiCNN_Ensemble.py
@@ -552,5 +552,5 @@ if __name__ == "__main__":
     final_model = cnn.to(device)
     final_model.load_state_dict(torch.load('models/ensemble.pt')['state_dict'])
     #
-    predict_on_my_test_set(final_model, mean_norm_test, std_norm_test, multiGPU=True)
-    predict_test_set_kaggle(final_model, test_filenames, mean_norm_test, std_norm_test, multiGPU=True)
+    predict_on_my_test_set(final_model, mean_norm_test, std_norm_test, multiGPU=False)
+    predict_test_set_kaggle(final_model, test_filenames, mean_norm_test, std_norm_test, multiGPU=False)
