b22e7ff9cefb3c5595cc8294a095dce25736bbfc
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-10 19:39:36 +0100

    reset_data

--- PIL_preprocessing.py
+++ PIL_preprocessing.py
@@ -20,15 +20,15 @@ classified_train_images = pickle.load(open("pkl/classified_train_images.pkl", "r
 
 def pad_and_resize(im):
     desired_size = 80
-    old_size = im.size  # old_size[0] is in (width, height) format
-    ratio = float(desired_size)/max(old_size)
-    new_size = tuple([int(x*ratio) for x in old_size])
-    im = im.resize(new_size, Image.ANTIALIAS)
-    new_im = Image.new("RGB", (desired_size, desired_size), "white")
-    new_im.paste(im, ((desired_size-new_size[0])//2,
-                        (desired_size-new_size[1])//2))
-    new_im = new_im.convert('L')
-    # new_im = im.resize((desired_size, desired_size), Image.ANTIALIAS)
+    # old_size = im.size  # old_size[0] is in (width, height) format
+    # ratio = float(desired_size)/max(old_size)
+    # new_size = tuple([int(x*ratio) for x in old_size])
+    # im = im.resize(new_size, Image.ANTIALIAS)
+    # new_im = Image.new("RGB", (desired_size, desired_size), "white")
+    # new_im.paste(im, ((desired_size-new_size[0])//2,
+    #                     (desired_size-new_size[1])//2))
+    # new_im = new_im.convert('L')
+    new_im = im.resize((desired_size, desired_size), Image.ANTIALIAS)
     return new_im
 
 preprocessed_train_images = []
@@ -46,6 +46,6 @@ for im in classified_train_images:
 train_images[78]
 preprocessed_train_images[78]
 
-pickle.dump( preprocessed_train_images, open( "pkl/train_padded80", "wb" ) )
-pickle.dump( preprocessed_test_images, open( "pkl/test_padded80.pkl", "wb" ) )
-pickle.dump( preprocessed_classified_images, open( "pkl/classified_padded80.pkl", "wb" ) )
+pickle.dump( preprocessed_train_images, open( "pkl/train_resized80", "wb" ) )
+pickle.dump( preprocessed_test_images, open( "pkl/test_resized80.pkl", "wb" ) )
+pickle.dump( preprocessed_classified_images, open( "pkl/classified_resized80.pkl", "wb" ) )
