852fc4235ed9de33051604d52080f25415c2afcf
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-10 19:42:07 +0100

    reset_data

--- PIL_preprocessing.py
+++ PIL_preprocessing.py
@@ -19,7 +19,7 @@ classified_train_images = pickle.load(open("pkl/classified_train_images.pkl", "r
 
 
 def pad_and_resize(im):
-    desired_size = 80
+    desired_size = 64
     # old_size = im.size  # old_size[0] is in (width, height) format
     # ratio = float(desired_size)/max(old_size)
     # new_size = tuple([int(x*ratio) for x in old_size])
@@ -46,6 +46,6 @@ for im in classified_train_images:
 train_images[78]
 preprocessed_train_images[78]
 
-pickle.dump( preprocessed_train_images, open( "pkl/train_resized80", "wb" ) )
-pickle.dump( preprocessed_test_images, open( "pkl/test_resized80.pkl", "wb" ) )
-pickle.dump( preprocessed_classified_images, open( "pkl/classified_resized80.pkl", "wb" ) )
+pickle.dump( preprocessed_train_images, open( "pkl/train_resized64", "wb" ) )
+pickle.dump( preprocessed_test_images, open( "pkl/test_resized64.pkl", "wb" ) )
+pickle.dump( preprocessed_classified_images, open( "pkl/classified_resized64.pkl", "wb" ) )
