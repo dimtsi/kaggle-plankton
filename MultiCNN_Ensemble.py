e452f0f9fbf1b2212cb5a476a85f0963779280fd
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-10 05:16:03 +0100

    new_additions

--- MultiCNN_Ensemble.py
+++ MultiCNN_Ensemble.py
@@ -454,38 +454,35 @@ if __name__ == "__main__":
 
     from sklearn.model_selection import StratifiedKFold
 
+    models = []
     pretrained = resnet50(pretrained = True)
     cnn1 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
+    cnn1_dict = torch.load('models/affine90.pt')['state_dict']
+    cnn1.load_state_dict(cnn1_dict)
+    models.append(cnn1)
 
     cnn2 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
+    cnn2_dict = torch.load('models/test_model15.pt')['state_dict']
+    cnn2.load_state_dict(cnn2_dict)
+    models.append(cnn2)
+
     cnn3 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
-    cnn4 = ResNetDynamic(pretrained.block, pretrained.layers,
-                num_layers = 2, pretrained_nn = None)
-    #
-    cnn1_dict = torch.load('models/affine90.pt')['state_dict']
-    cnn2_dict = torch.load('models/test_model15.pt')['state_dict']
     cnn3_dict = torch.load('models/test_model3.pt', map_location={'cuda:1': 'cuda:0'})['state_dict']
-    cnn4_dict = torch.load('models/test_model90.pt', map_location={'cuda:2': 'cuda:0'})['state_dict']
-
-    cnn1.load_state_dict(cnn1_dict)
-    cnn2.load_state_dict(cnn2_dict)
     cnn3.load_state_dict(cnn3_dict)
-    cnn4.load_state_dict(cnn3_dict)
-
-
-    # cnn2 = ResNetDynamic(Bottleneck, [2, 2, 2, 3],num_layers = 4)
-    models = []
-    models.append(cnn1)
-    # models.append(cnn2)
     models.append(cnn3)
 
+    cnn4 = ResNetDynamic(pretrained.block, pretrained.layers,
+                num_layers = 2, pretrained_nn = None)
+    cnn4_dict = torch.load('models/test_model90.pt', map_location={'cuda:2': 'cuda:0'})['state_dict']
+    cnn4.load_state_dict(cnn3_dict)
+    models.append(cnn4)
+
+    # cnn2 = ResNetDynamic(Bottleneck, [2, 2, 2, 3],num_layers = 4)
     cnn = EnsembleClassifier(models)
     # cnn1_dict = torch.load('ensemble.pt')['state_dict']
-
-
 
     def train_ensemble_on_test():
         norm = {}
