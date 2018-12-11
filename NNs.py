cc6609ef58c87075088760d0ed93f7e1005a7f91
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-11 21:29:02 +0100

    all

--- NNs.py
+++ NNs.py
@@ -232,20 +232,20 @@ class EnsembleClassifier(nn.Module):
         self.multiGPU = multiGPU
         super(type(self), self).__init__()
         self.net1 =  nn.Sequential(*list(networks[0].children()))
-        # self.net1.requires_grad = False
+        self.net1.requires_grad = False
         self.net2 =  nn.Sequential(*list(networks[1].children()))#[:-1]
-        # self.net2.requires_grad = False
+        self.net2.requires_grad = False
         self.net3 =  nn.Sequential(*list(networks[2].children()))#[:-1]
-        # self.net3.requires_grad = False
+        self.net3.requires_grad = False
         self.net4 =  nn.Sequential(*list(networks[3].children()))#[:-1]
-        # self.net4.requires_grad = False
+        self.net4.requires_grad = False
 
         self.fusion = Fusion()
         self.final_size = 0
         for net in networks:
             self.final_size += num_classes
         self.fc1 = nn.Sequential(
-            nn.Linear(self.final_size, (int)(1.2*self.final_size)),
+            nn.Linear(self.final_size, (int)(1*self.final_size)),
             nn.LeakyReLU(0.3),
             nn.Dropout(0.6)
             )
