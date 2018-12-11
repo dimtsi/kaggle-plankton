375fb862a85ddc345453445bc13cb99c55f11c48
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-11 19:24:32 +0100

    all

--- NNs.py
+++ NNs.py
@@ -258,8 +258,8 @@ class EnsembleClassifier(nn.Module):
             x3 = self.net3(x.to(self.devices[2]))
             x4 = self.net4(x.to(self.devices[3]))
             z = self.fusion([x1, x2.to(self.devices[0]),
-                            x3.to(self.devices[0])])
-                            #, x4.to(self.devices[0])
+                            x3.to(self.devices[0]),
+                            x4.to(self.devices[0])])
 
         else:
             x1 = self.net1(x)
