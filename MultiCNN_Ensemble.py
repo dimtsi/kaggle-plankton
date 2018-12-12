b6903907e3f54199ecef33f17b733a6b0644ce5e
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-12 22:33:54 +0100

    all

--- MultiCNN_Ensemble.py
+++ MultiCNN_Ensemble.py
@@ -634,9 +634,9 @@ if __name__ == "__main__":
         trained_model = train_and_validate(cnn, train_loader, test_loader,
                                            num_epochs=200, device = device,
                                            multiGPU = True,
-                                           save_name = 'extraclassified/ensemble_1fc_whole.pt')
+                                           save_name = 'extraclassified/ensemble.pt')
 
-    train_ensemble_on_whole_test_mine()
+    # train_ensemble_on_whole_test_mine()
 
 
     def train_ensemble_on_test():
@@ -661,10 +661,10 @@ if __name__ == "__main__":
         trained_model = train_and_validate(cnn, train_loader, test_loader,
                                            num_epochs=200, device = device,
                                            multiGPU = True,
-                                           save_name = 'extraclassified/ensemble_1fc.pt')
+                                           save_name = 'extraclassified/ensemble.pt')
     print(cnn)
     # # cnn.to(device)
-    # train_ensemble_on_test()
+    train_ensemble_on_test()
     #
     # mean_norm_test, std_norm_test = calc_means_stds(train_images)
     #
