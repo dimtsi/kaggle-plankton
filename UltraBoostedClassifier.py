33e6552dde2811ec75b70364e3f99f3ba3d32a4a
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2018-12-12 20:13:02 +0100

    all

--- UltraBoostedClassifier.py
+++ UltraBoostedClassifier.py
@@ -579,19 +579,19 @@ if __name__ == "__main__":
 
     cnn1 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
-    cnn1_dict = torch.load('models/test_model3.pt')['state_dict']
+    cnn1_dict = torch.load('models/trained_model3.pt')['state_dict']
     cnn1.load_state_dict(cnn1_dict)
     models.append(cnn1)
 
     cnn2 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
-    cnn2_dict = torch.load('models/test_model15.pt')['state_dict']
+    cnn2_dict = torch.load('models/trained_model15.pt')['state_dict']
     cnn2.load_state_dict(cnn2_dict)
     models.append(cnn2)
 
     cnn3 = ResNetDynamic(pretrained.block, pretrained.layers,
                 num_layers = 2, pretrained_nn = None)
-    cnn3_dict = torch.load('models/test_model90.pt')['state_dict']
+    cnn3_dict = torch.load('models/trained_model90.pt')['state_dict']
     cnn3.load_state_dict(cnn3_dict)
     models.append(cnn3)
 
